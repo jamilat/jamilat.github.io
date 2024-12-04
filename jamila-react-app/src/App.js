39d837bfd9990ab64622af28070585e04b872495
Jamila <jamila9tomines@gmail.com>
2024-12-04 21:34:21 +1100

    Semi-customized

--- jamila-react-app/src/App.js
+++ jamila-react-app/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You've reached <code>/Users/jamila</code> Welcome!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://techbytedesign.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tech-Byte-Design
         </a>
       </header>
     </div>
